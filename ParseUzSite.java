6e1c666146af0c69094f6b909508d7ae6009b98c
XeonAH <88236@stud.uz.zgora.pl>
2016-06-11 18:12:42 +0200

    work with NP groups too

--- ParseUzSite.java
+++ ParseUzSite.java
@@ -10,128 +10,88 @@ import org.jsoup.select.Elements;
 
 
 
-
 public class ParseUzSite {
-	static ArrayList<String> startHourArray = new ArrayList<>();
-	static ArrayList<String> endHourArray = new ArrayList<>();
-	static ArrayList<String> name = new ArrayList<>();
-	public static void main(String args[]) throws IOException {
+	ArrayList<String> startHourArray = new ArrayList<>();
+	ArrayList<String> endHourArray = new ArrayList<>();
+	ArrayList<String> name = new ArrayList<>();
+	String url = null;
+	
+	public ParseUzSite(String url) throws IOException {
+		setUrl(url);
+		parseUz();
+	}
+	
+	public void parseUz() throws IOException {
 		try {
-			Document uzPlan = Jsoup.connect("http://plan.uz.zgora.pl/grupy_plan.php?pId_Obiekt=16670").get();
-			Document uzTerm = Jsoup.connect("http://plan.uz.zgora.pl/kalendarze_lista_szczegoly.php?pId_kalendarz=1612").get();
+			Document uzPlan = Jsoup.connect(url).get();
 			Elements days = uzPlan.select("p");
-			
+			Document uzTerm = Jsoup.connect("http://plan.uz.zgora.pl/" + 
+											days.get(0).parent().parent().nextElementSibling().child(7).child(0).attr("href")).get();
 			String startHour;
 			String endHour;
 			Element temp;
 			String date;
 			Element dates;
+			String helpDate;
+			int k;
 			for(int i = 0; i < days.size(); i++) {
-		//		System.out.println(days.get(i).text());
+				k = 1;
 				temp = days.get(i).parent().parent();
 				while(true) {
 					temp = temp.nextElementSibling();
 					try {
 						startHour = temp.child(1).text();
 					}
-					catch(Exception e) {
+					catch(Exception e) {	
 						break;
 					}
 					endHour = temp.child(2).text();
 					date = temp.child(7).text();
-					if( date.equals("D/") ) {
-						dates = uzTerm.select("table").get(1).child(0).child(1);
-						while(true) {
-							try {	
-								if(dates.child(2).text().equals(days.get(i).text())) {
-								startHourArray.add(dates.child(1).text() + " " + 
-												   startHour);
-								endHourArray.add(dates.child(1).text() + " " + 
-										   endHour);
-								name.add(temp.child(3).text());
-	
-								}
-								dates = dates.nextElementSibling();
-							}
-							catch(Exception e) {break;}
-						}
-					}
-					else if( date.equals("DI/") ) {
-						dates = uzTerm.select("table").get(2).child(0).child(1);
-						while(true) {
-							try {	
-								if(dates.child(2).text().equals(days.get(i).text())) {
-								startHourArray.add(dates.child(1).text() + " " + 
-												   startHour);
-								endHourArray.add(dates.child(1).text() + " " + 
-										   endHour);
-								name.add(temp.child(3).text());
-	
-								}
-								dates = dates.nextElementSibling();
-							}
-							catch(Exception e) {break;}
-						}
-					}
-					if( date.equals("DII/") ) {
-						dates = uzTerm.select("table").get(3).child(0).child(1);
-						while(true) {
-							try {	
-								if(dates.child(2).text().equals(days.get(i).text())) {
-								startHourArray.add(dates.child(1).text() + " " + 
-												   startHour);
-								endHourArray.add(dates.child(1).text() + " " + 
-										   endHour);
-								name.add(temp.child(3).text());
-	
-								}
-								dates = dates.nextElementSibling();
-							}
-							catch(Exception e) {break;}
-						}
-					}
-					if( date.equals("DN/") ) {
-						dates = uzTerm.select("table").get(4).child(0).child(1);
-						while(true) {
-							try {	
-								if(dates.child(2).text().equals(days.get(i).text())) {
-								startHourArray.add(dates.child(1).text() + " " + 
-												   startHour);
-								endHourArray.add(dates.child(1).text() + " " + 
-										   endHour);
-								name.add(temp.child(3).text());
-	
-								}
-								dates = dates.nextElementSibling();
-							}
-							catch(Exception e) {break;}
-						}
-					}
-					if( date.equals("DP/") ) {
-						dates = uzTerm.select("table").get(4).child(0).child(1);
-						while(true) {
-							try {	
-								if(dates.child(2).text().equals(days.get(i).text())) {
-								startHourArray.add(dates.child(1).text() + " " + 
-												   startHour);
-								endHourArray.add(dates.child(1).text() + " " + 
-										   endHour);
-								name.add(temp.child(3).text());
-	
-								}
-								dates = dates.nextElementSibling();
-							}
-							catch(Exception e) {break;}
-						}
-					}
-					else {
+					date = date.substring(0, date.length()-1);
+					while(true) {
 						try {
-						date = date.substring(0,10);
+							dates = uzTerm.select("table").get(1).child(0).child(1);;
+							helpDate = uzTerm.select("table").get(k).parent().previousElementSibling().text();
 						}
-						catch(Exception e) {break;}
-						startHourArray.add(date + " " + startHour);
-						endHourArray.add(date + " " + endHour);
-						name.add(temp.child(3).text());
+						catch(Exception e) {
+							if(date.charAt(0) == '0' || date.charAt(0) == '1' ||
+									date.charAt(0) == '2' || date.charAt(0) == '3' ) {
+								try {
+									date = date.substring(0,10);
+									}
+									catch(Exception e2) {}
+									startHourArray.add(date + " " + startHour);
+									endHourArray.add(date + " " + endHour);
+									name.add(temp.child(3).text());
+							}
+							k = 1;
+							break;
+							
+						}
+						k++;
+						if(helpDate.substring(0,13).equals("Terminy zajêæ")) {
+							helpDate = helpDate.substring(14, helpDate.indexOf(" - "));
+						}
+						else {
+							helpDate = helpDate.substring(0, helpDate.indexOf(" - "));
+						}
+						if(date.equals(helpDate)) {
+							while(true) {
+								try {
+
+									if(dates.child(2).text().equals(days.get(i).text())) {
+										startHourArray.add(dates.child(1).text() + " " + 
+													   	startHour);
+										endHourArray.add(dates.child(1).text() + " " + 
+														 endHour);
+										name.add(temp.child(3).text());	
+									}
+									dates = dates.nextElementSibling();
+								}
+								catch(Exception e) {
+									break;}
+							}
+						}	
 					}
 				}
 			}
@@ -149,7 +109,10 @@ public class ParseUzSite {
 	public ArrayList<String> getName() {
 		return name;
 	}
-	static public void printEvents() {
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public void printEvents() {
 		for(int i = 0; i < startHourArray.size(); i++)
 			System.out.println(startHourArray.get(i) + " - " +
 							   endHourArray.get(i) + " " +
