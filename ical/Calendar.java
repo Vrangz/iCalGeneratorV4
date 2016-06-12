8724c3052ebd5ef90ba14867e9edcf6fd4bf910d
Marcin W <iim1@wp.pl>
2016-06-12 17:46:54 +0200

    Dodano komentarze.

--- ical/Calendar.java
+++ ical/Calendar.java
@@ -2,80 +2,137 @@ package ical;
 
 import java.util.ArrayList;
 
+/**
+ * Opisuje kalendarz.
+ */
 class Calendar {
     
+	/**
+	 * Tworzy nowy kalendarz z pustymi wydarzeniami.
+	 */
     public Calendar() {
         events = new ArrayList<>();
         events.add(new Event());
     }
 
+	/**
+	 * Zwraca nazwę kalendarza.
+	 */
     public String getName() {
         return "test";
     }
 
+	/**
+	 * Zwraca strefę czasową kalendarza.
+	 */
     public String getTimeZone() {
         return "Europe/Berlin";
     }
 
+	/**
+	 * Zwraca godzinę obowiązywania czasu letniego.
+	 */
     public String getDaylightFrom() {
         return "+0100";
     }
 
+	/**
+	 * Zwraca godzinę zakończenia czasu letniego.
+	 */
     public String getDaylightTo() {
         return "+0200";
     }
 
+	/**
+	 * Zwraca strefę czasową czasu letniego.
+	 */
     public String getDaylightName() {
         return "CEST";
     }
 
+	/**
+	 * Zwraca godzinę obowiązywania czasu standardowego.
+	 */
     public String getStandardFrom() {
         return "+0200";
     }
 
+	/**
+	 * Zwraca godzinę zakończenia czasu standardowego.
+	 */
     public String getStandardTo() {
         return "+0100";
     }
 
+	/**
+	 * Zwraca strefę czasową czasu standardowego.
+	 */
     public String getStandardName() {
         return "CET";
     }
 
+	/**
+	 * Zwraca wszystkie wydarzenia z kalendarza.
+	 */
     public ArrayList<Event> getEvents() {
         return events;
     }
 
+	/**
+	 * Ustawia nazwę kalendarza.
+	 */
     void setName(String value) {
         System.out.println("X-WR-CALNAME:" + value);
     }
 
+	/**
+	 * Ustawia strefę czasową kalendarza.
+	 */
     void setTimeZone(String value) {
         System.out.println("TZID:" + value);
     }
 
+	/**
+	 * Ustawia godzinę obowiązywania czasu letniego.
+	 */
     void setDaylightFrom(String value) {
         System.out.println("BEGIN:DAYLIGHT");
         System.out.println("TZOFFSETFROM:" + value);
     }
 
+	/**
+	 * Ustawia godzinę zakończenia czasu letniego.
+	 */
     void setDaylightTo(String value) {
         System.out.println("TZOFFSETTO:" + value);
     }
 
+	/**
+	 * Ustawia strefę czasową czasu letniego.
+	 */
     void setDaylightName(String value) {
         System.out.println("TZNAME:" + value);
         System.out.println("END:DAYLIGHT");
     }
 
+	/**
+	 * Ustawia godzinę obowiązywania czasu standardowego.
+	 */
     void setStandardFrom(String value) {
         System.out.println("BEGIN:STANDARD");
         System.out.println("TZOFFSETFROM:" + value);
     }
 
+	/**
+	 * Ustawia godzinę zakończenia czasu standardowego.
+	 */
     void setStandardTo(String value) {
         System.out.println("TZOFFSETTO:" + value);
     }
 
+	/**
+	 * Ustawia strefę czasową czasu standardowego.
+	 */
     void setStandardName(String value) {
         System.out.println("TZNAME:" + value);
         System.out.println("END:STANDARD");
--- ical/Event.java
+++ ical/Event.java
@@ -1,67 +1,118 @@
 package ical;
 
+/**
+ * Opisuje jedno zdarzenie w kalendarzu.
+ */
 class Event {
 
+	/**
+	 * Tworzy puste wydarzenie.
+	 */
     public Event() {
     }
 
+	/**
+	 * Zwraca datę początkową.
+	 */
     public String getDateDTS() {
         return "20160302T174116Z";
     }
 
+	/**
+	 * Zwraca unikalne ID wydarzenia.
+	 */
     public String getUID() {
         return "1@example.com";
     }
 
+	/**
+	 * Zwraca datę rozpoczęcia wydarzenia.
+	 */
     public String getDate() {
         return "20160302T120000";
     }
 
+	/**
+	 * Zwraca datę zakończenia wydarzenia.
+	 */
     public String getDateEnd() {
         return "20160316T121500";
     }
 
+	/**
+	 * Zwraca nazwę wydarzenia.
+	 */
     public String getTitle() {
         return "eewe";
     }
 
+	/**
+	 * Zwraca opis wydarzenia.
+	 */
     public String getDescription() {
         return "weweew";
     }
 
+	/**
+	 * Zwraca czy jest ustawiony alarm na wydarzeniu.
+	 */
     public boolean hasAlarm() {
         return true;
     }
 
+	/**
+	 * Zwraca godzinę ustawionego alarmu.
+	 */
     public String getTrigger() {
         return "15M";
     }
 
+	/**
+	 * Ustawia początkową datę.
+	 */
     public void setDateDTS(String value) {
         System.out.println("BEGIN:VEVENT");
         System.out.println("DTSTAMP:" + value);
     }
 
+	/**
+	 * Ustawia unikalne ID wydarzenia.
+	 */
     public void setUID(String value) {
         System.out.println("UID:" + value);
     }
 
+	/**
+	 * Ustawia datę rozpoczęcia wydarzenia.
+	 */
     public void setDate(String value) {
         System.out.println("DTSTART:" + value);
     }
 
+	/**
+	 * Ustawia datę zakończenia wydarzenia.
+	 */
     public void setDateEnd(String value) {
         System.out.println("DTEND:" + value);
     }
 
+	/**
+	 * Ustawia nazwę wydarzenia.
+	 */
     public void setTitle(String value) {
         System.out.println("SUMMARY:" + value);
     }
 
+	/**
+	 * Ustawia opis wydarzenia.
+	 */
     public void setDescription(String value) {
         System.out.println("DESCRIPTION:" + value);
     }
 
+	/**
+	 * Ustawia alarm na wydarzeniu.
+	 */
     public void setAlarm(String value) {
         System.out.println("TRIGGER:" + value);
         System.out.println("END:VEVENT");
